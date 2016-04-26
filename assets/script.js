79b973c84bbca5b366416f6d7c23d5b0917a2f22
Jakob Fix <jakob@jfix.com>
2016-04-27 00:12:14 +0200

    works now also for firefox

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,83 @@
 $(function(){
 
-var cvr, hdl, img;
+  var hdl, img;
 
-function handleFileSelect(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
+  function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
 
-  var file = evt.dataTransfer.files[0];
-  var imgDiv = document.getElementById('image');
-  var reader = new FileReader();
-  reader.onload = (function(theFile) {
-    return function(e) {
-      imgDiv.innerHTML = "<img id='coverimage' src='"+ e.target.result +"' title='"+escape(theFile.name)+"'/>"
-      img = $("#coverimage")[0];
-      var w = img.clientWidth;
-      var h = img.clientHeight;
+    var file = evt.dataTransfer.files[0],
+        imgDiv = $('#image'),
+        reader = new FileReader();
 
-      // 850px x 1024px
-      var vpw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-      var vph = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    reader.onload = (function(theFile) {
+      return function(e) {
+        imgDiv.prepend("<img id='coverimage' src='"+ e.target.result +"' title='"+escape(theFile.name)+"'/>");
+        img = $("#coverimage");
+        var w = img.width(); //img.clientWidth;
+        var h = img.height(); //img.clientHeight;
 
-      hdl = $("#handle");
-      cvr = $("#coverimage");
+        // 850px x 1024px
+        var vpw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var vph = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-      if (h > vph || w >vph) {
-        img.style.top = "300px";
-        img.style.left = "300px";
-        img.style.height = "500px";
-        img.style.width = "auto";
-        hdl.css("height", "500px").css("width", "auto");
-        hdl.css("top", "300px").css("left", "300px");
-      } else {
-        img.style.top = Math.round((vph - h) / 2) + "px";
-        img.style.left = Math.round((vpw - w) / 2) + "px";
+        hdl = $("#handle");
+
+        if (h > vph || w >vpw) {
+          img.css("top", "300px").css("left", "300px").css("height", "500px").css("width", "auto");
+          hdl.css("height", "500px").css("width", "auto").css("top", "300px").css("left", "300px");
+        } else {
+          img
+            .css("top", Math.round((vph - h) / 2) + "px")
+            .css("left", Math.round((vpw - w) / 2) + "px");
+          hdl
+            .css("top", Math.round((vph - h) / 2) + "px")
+            .css("left", Math.round((vpw - w) / 2) + "px");
+        }
+
         hdl
-          .css("top", Math.round((vph - h) / 2) + "px")
-          .css("left", Math.round((vpw - w) / 2) + "px");
-      }
+          .css('height', img.height())
+          .css('width', img.width())
+          .css("display", "block");
 
-      hdl
-        .css('height', cvr.height())
-        .css('width', cvr.width())
-        .css("display", "block");
+          hdl.resizable({
+            aspectRatio: true,
+            autoHide: true,
+            alsoResize: "#coverimage",
+            handles: 'all',
+            resize: function(event, ui) {
+              img
+                .css('left', ui.position.left)
+                .css('top', ui.position.top);
+            }
+          });
 
-        hdl.resizable({
-          aspectRatio: true,
-          autoHide: true,
-          alsoResize: "#coverimage",
-          handles: 'all',
-          resize: function(event, ui) {
-            cvr
-              .css('left', ui.position.left)
-              .css('top', ui.position.top);
-          }
-        });
+          hdl.draggable({
+            // containment: "document",
+            drag: function(event, ui) {
+              img
+                .css('left', ui.position.left)
+                .css('top', ui.position.top);
+            }
+          });
+      };
+    })(file);
 
-        hdl.draggable({
-          // containment: "document",
-          drag: function(event, ui) {
-            cvr
-              .css('left', ui.position.left)
-              .css('top', ui.position.top);
-          }
-        });
-    };
-  })(file);
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(file);
+    // destroy dropZone ...
+    dropZone.parentNode.removeChild(dropZone);
+  }
 
-  // Read in the image file as a data URL.
-  reader.readAsDataURL(file);
-  // destroy dropZone ...
-  dropZone.parentNode.removeChild(dropZone);
-}
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
 
-function handleDragOver(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-  evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-}
-
-// Setup the dnd listeners.
-var dropZone = document.getElementById('dropzone');
-dropZone.addEventListener('dragover', handleDragOver, false);
-dropZone.addEventListener('drop', handleFileSelect, false);
+  // Setup the dnd listeners.
+  var dropZone = document.getElementById('dropzone');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
 
 });
